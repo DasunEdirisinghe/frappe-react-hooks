4b087f1d10c804b77897cae21279aa0f9017b997
DasunEdirisinghe <edirisinghe954@gmail.com>
2024-02-06 23:17:19 +0530

    feat: new key invalidator

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { useState } from 'react'
-import { useFrappeAuth, useFrappeCreateDoc, useFrappeGetDocList, useSWRConfig } from './lib'
-import { Test } from './Test'
 
 function App() {
-	const [count, setCount] = useState(0)
-	const { login } = useFrappeAuth()
-
-
-	const onLogin = () => {
-		login({
-			username: "Administrator",
-			password: 'dlad'
-		})
-	}
-
-	const { data = [],} = useFrappeGetDocList<any>("Warehouse", {
-		fields: ["name"],
-	}, "item-master-list", {})
-
-	const { data: d = []} = useFrappeGetDocList<any>("Item", {
-		fields: ["name"],
-	}, "items", {})
-
-
 	return (
 		<div className="App">
-			<button onClick={onLogin}>Login</button>
-			
-			{data?.map((item) => <div key={item.name}>{item.name}</div>)}
-			<hr />
-			{d?.map((item) => <div key={item.name}>{item.name}</div>)}
-
-			<Test />
 		</div>
 	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { FrappeProvider } from './lib'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <FrappeProvider url='http://127.0.0.1:8012' enableSocket={false}>
+    <FrappeProvider>
       <App />
     </FrappeProvider>
   </React.StrictMode>
